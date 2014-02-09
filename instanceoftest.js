c767f21576cf7d014f36673e59e1e749f03a4c34
yanhuang <yanhuang@deepglint.com>
2014-02-09 13:43:20 +0800

    add the singleton test demo

--- instanceoftest.js
+++ instanceoftest.js
@@ -5,4 +5,23 @@ var a=new Test1();
 var b=new Test2();
 console.log(a instanceof Test1);
 console.log(a instanceof Test2);
-/console.log(a.prototype.isPrototypeOf(b));
+/*console.log(a.prototype.isPrototypeOf(b));
+*/
+
+function typeAndValue(x){
+	if(x==null) return "";
+	switch(x.constructor){
+		case Number: return "Number: "+x;
+		case String: return "String: "+x;
+		case Date: return "Date: "+x;
+		case Test2: return "Test2: ";
+		case Test1: return "Test1: ";
+		default: return "Other";
+	}
+}
+function Test3(){}
+var c=new Test3();
+
+console.log(typeAndValue(a));
+console.log(typeAndValue(12));
+console.log(typeAndValue(c));
---
+++ singleton.js
@@ -0,0 +1,32 @@
+/*
+In this singleton instance all the members are public,so all the 
+variables will be loaded when instance is created
+*/
+var singleton1={
+	a:1,
+	b:2,
+	c:function(){console.log('helloworld');},
+	d:function(d){console.log(d)}
+}
+
+var singleton2=function(){
+	var a=1,fn=function(){};
+	return {
+		method:function(){fn;},
+		getAttr:function(){return a;}
+	};
+}();
+
+var LazySingleton=function(){
+	var a=1,fn=function(){};
+	var obj={
+		method:function(){fn()},
+		getAttr:function(){return a;}
+	};
+	function init(){
+		return obj;
+	}
+	return {
+		getInstance:init
+	};
+}
